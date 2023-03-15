34a9fe8d94fcfd505e330ccfc815626ba5e3547d
taiki-umetsu <taiki.umetsu.st@gmail.com>
2023-03-16 01:07:51 +0900

    add text about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,44 @@
 import React from "react";
-import { Text, Link as ChakraLink, Box } from "@chakra-ui/react";
+import {
+  Text,
+  Link as ChakraLink,
+  Box,
+  Heading,
+  VStack,
+} from "@chakra-ui/react";
 import Layout from "@/components/Layout";
 
 const IndexPage: React.FC = () => {
   return (
     <Layout>
       <Box>
-        <Text textStyle="p">
-          I&apos;m a Backend developer with 2+ years of experience in Ruby on
-          Rails. &#128512;
-        </Text>
-        <ChakraLink
-          href="/experience"
-          _hover={{ textDecoration: "none" }}
-          style={{ color: "white" }}
-        >
-          <Text textStyle="p" _hover={{ color: "gray.400" }}>
-            Read more{" "}
-            <Text as="span" fontSize="sm">
-              →
-            </Text>
+        <VStack align="start" textStyle="p">
+          <Heading as="h1">Hi, I&apos;m Taiki Umetsu &#128075;</Heading>
+
+          <Text pb="10">
+            A dedicated backend developer with over 2 years of experience and a
+            passion for problem-solving and creating efficient solutions. I have
+            experience working with Ruby on Rails, JavaScript, SQL, and AWS. I
+            take pride in my ability to learn and adapt quickly, as well as my
+            commitment to delivering high-quality work to my clients and team
+            members. I enjoy dedicating time to learning about new technologies
+            and honing my skills to stay ahead in this rapidly evolving field.
+          </Text>
+
+          <Text pb="10">
+            Apart from my passion for backend development, I love traveling and
+            exploring new cultures. My journey around Asia and my time spent
+            living in Sydney have enriched my life experiences and broadened my
+            perspective. I enjoy taking on challenges, such as reaching the
+            Everest Base Camp in Nepal.
+          </Text>
+
+          <Text pb="10">Currently, I&apos;m seeking a job in Vancouver!</Text>
+        </VStack>
+        <ChakraLink href="/experience" textStyle="p">
+          Read more{" "}
+          <Text as="span" fontSize="sm">
+            →
           </Text>
         </ChakraLink>
       </Box>
